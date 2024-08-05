c3aafb6fff07cf60c2fe60edbfcbab15ddaaffb2
@ronaldo-silva2_Hon <ronaldo.silva2@honeywell.com>
2024-08-05 09:55:02 -0300

    tile edit for testing

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kaizen',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Kaizen'),
     );
   }
 }
