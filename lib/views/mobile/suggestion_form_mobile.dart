b0cedddcee40aeb4d236295c01e4928f996b3a94
@ronaldo-silva2_Hon <ronaldo.silva2@honeywell.com>
2024-08-09 13:35:39 -0300

    202508091335

--- lib/views/mobile/suggestion_form_mobile.dart
+++ lib/views/mobile/suggestion_form_mobile.dart
@@ -62,243 +62,242 @@ class _SuggestionFormMobileState extends State<SuggestionFormMobile> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Center(
+    return Center(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
         child: SizedBox(
-          width: 500,
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(right: 20.0, left: 20.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      // crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Center(
-                          child: Image.asset(
+          width: 400,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 20.0, left: 20.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset(
                             'assets/images/kaizen_logo.png',
                             height: 200,
                             width: 200,
                           ),
-                        ),
-                        const SizedBox(height: 16),
-                      ],
-                    ),
-
-                    // const SizedBox(height: 32),
-                    Text('suggestion_register'.tr,
-                        style:
-                            const TextStyle(fontSize: 20, color: Colors.grey)),
-                    const Divider(
-                      thickness: 1,
-                    ),
-                    const SizedBox(height: 16),
-
-                    CustomTextFormField(
-                        width: 300,
-                        hintText: 'hint_eid'.tr,
-                        labelText: 'EID',
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'validator_eid'.tr;
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          _eid = value;
-                        },
-                        maxLines: 1),
-
-                    const SizedBox(height: 16),
-
-                    CustomDropdownButtonFormField(
-                      width: 300,
-                      value: _selectedArea,
-                      hintText: 'hint_area'.tr,
-                      labelText: 'label_area'.tr,
-                      items: _areas,
-                      onChanged: (newValue) {
-                        setState(() {
-                          _selectedArea = newValue;
-                        });
-                      },
-                      validator: (value) {
-                        if (value == null) {
-                          return 'validator_area'.tr;
-                        }
-                        return null;
-                      },
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    CustomDropdownButtonFormField(
-                      width: 300,
-                      value: _selectedCategory,
-                      hintText: 'hint_category'.tr,
-                      labelText: 'label_category'.tr,
-                      items: _categorias,
-                      onChanged: (newValue) {
-                        setState(() {
-                          _selectedCategory = newValue;
-                        });
-                      },
-                      validator: (value) {
-                        if (value == null) {
-                          return 'validator_category'.tr;
-                        }
-                        return null;
-                      },
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    CustomDropdownButtonFormField(
-                      width: 300,
-                      value: _selectedSubcategory,
-                      hintText: 'hint_subcategory'.tr,
-                      labelText: 'label_subcategory'.tr,
-                      items: _subcategorias,
-                      onChanged: (newValue) {
-                        setState(() {
-                          _selectedSubcategory = newValue;
-                        });
-                      },
-                      validator: (value) {
-                        if (value == null) {
-                          return 'validator_subcategory'.tr;
-                        }
-                        return null;
-                      },
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    // ),
-
-                    CustomDropdownButtonFormField(
-                      width: 300,
-                      value: _selectedCandidate,
-                      hintText: 'hint_option'.tr,
-                      labelText: 'label_option'.tr,
-                      items: ['yes'.tr, 'no'.tr],
-                      onChanged: (newValue) {
-                        setState(() {
-                          _selectedCandidate = newValue;
-                        });
-                      },
-                      validator: (value) {
-                        if (value == null) {
-                          return 'validator_option'.tr;
-                        }
-                        return null;
-                      },
-                    ),
-
-                    const SizedBox(height: 16),
-
-                    CustomTextFormField(
-                        width: 400,
-                        hintText: 'hint_description'.tr,
-                        labelText: 'label_description'.tr,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'validator_description'.tr;
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          _problemDescription = value;
-                        },
-                        maxLines: 8),
-
-                    const SizedBox(height: 16),
-
-                    CustomTextFormField(
-                        width: 400,
-                        hintText: 'hint_action'.tr,
-                        labelText: 'label_action'.tr,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'validator_action'.tr;
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          _suggestion = value;
-                        },
-                        maxLines: 8),
-
-                    const SizedBox(height: 16),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ElevatedButton.icon(
-                                onPressed: () {
-                                  // Implement file picker
-                                },
-                                icon: const Icon(Icons.attach_file),
-                                label: Text('label_ttach_image'.tr),
-                              ),
-                              const SizedBox(height: 16),
-                              ElevatedButton(
-                                onPressed: () {
-                                  if (_formKey.currentState!.validate()) {
-                                    // Process data
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                          content:
-                                              Text('snack_send_suggestion'.tr)),
-                                    );
-                                  }
-                                },
-                                child: Text('button_send'.tr),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            //  mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              const SizedBox(height: 16),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Implement suggestion tracking
-                                },
-                                child: Text('button_suggestion'.tr),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 50),
-                    const Divider(
-                      // color: Colors.grey, // Cor da linha
-                      thickness: 1, // Espessura da linha
-                    ),
-                    const SizedBox(height: 50),
-                    const Center(
-                      child: Text(
-                        '© Honeywell 2025',
-                        style: TextStyle(fontSize: 20),
+                          const SizedBox(height: 16),
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                  ],
+
+                      // const SizedBox(height: 32),
+                      Text('suggestion_register'.tr,
+                          style:
+                              const TextStyle(fontSize: 20, color: Colors.grey)),
+                      const Divider(
+                        thickness: 1,
+                      ),
+                      const SizedBox(height: 16),
+
+                      CustomTextFormField(
+                          width: 400,
+                          hintText: 'hint_eid'.tr,
+                          labelText: 'EID',
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'validator_eid'.tr;
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _eid = value;
+                          },
+                          maxLines: 1),
+
+                      const SizedBox(height: 16),
+
+                      CustomDropdownButtonFormField(
+                        width: 400,
+                        value: _selectedArea,
+                        hintText: 'hint_area'.tr,
+                        labelText: 'label_area'.tr,
+                        items: _areas,
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedArea = newValue;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null) {
+                            return 'validator_area'.tr;
+                          }
+                          return null;
+                        },
+                      ),
+
+                      const SizedBox(height: 16),
+
+                      CustomDropdownButtonFormField(
+                        width: 400,
+                        value: _selectedCategory,
+                        hintText: 'hint_category'.tr,
+                        labelText: 'label_category'.tr,
+                        items: _categorias,
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedCategory = newValue;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null) {
+                            return 'validator_category'.tr;
+                          }
+                          return null;
+                        },
+                      ),
+
+                      const SizedBox(height: 16),
+
+                      CustomDropdownButtonFormField(
+                        width: 400,
+                        value: _selectedSubcategory,
+                        hintText: 'hint_subcategory'.tr,
+                        labelText: 'label_subcategory'.tr,
+                        items: _subcategorias,
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedSubcategory = newValue;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null) {
+                            return 'validator_subcategory'.tr;
+                          }
+                          return null;
+                        },
+                      ),
+
+                      const SizedBox(height: 16),
+
+                      // ),
+
+                      CustomDropdownButtonFormField(
+                        width: 400,
+                        value: _selectedCandidate,
+                        hintText: 'hint_option'.tr,
+                        labelText: 'label_option'.tr,
+                        items: ['yes'.tr, 'no'.tr],
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedCandidate = newValue;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null) {
+                            return 'validator_option'.tr;
+                          }
+                          return null;
+                        },
+                      ),
+
+                      const SizedBox(height: 16),
+
+                      CustomTextFormField(
+                          width: 400,
+                          hintText: 'hint_description'.tr,
+                          labelText: 'label_description'.tr,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'validator_description'.tr;
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _problemDescription = value;
+                          },
+                          maxLines: 8),
+
+                      const SizedBox(height: 16),
+
+                      CustomTextFormField(
+                          width: 400,
+                          hintText: 'hint_action'.tr,
+                          labelText: 'label_action'.tr,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'validator_action'.tr;
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _suggestion = value;
+                          },
+                          maxLines: 8),
+
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                ElevatedButton.icon(
+                                  onPressed: () {
+                                    // Implement file picker
+                                  },
+                                  icon: const Icon(Icons.attach_file),
+                                  label: Text('label_ttach_image'.tr),
+                                ),
+                                const SizedBox(height: 16),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                      // Process data
+                                      ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(
+                                            content:
+                                                Text('snack_send_suggestion'.tr)),
+                                      );
+                                    }
+                                  },
+                                  child: Text('button_send'.tr),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              //  mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                const SizedBox(height: 16),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    // Implement suggestion tracking
+                                  },
+                                  child: Text('button_suggestion'.tr),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(height: 50),
+                      const Divider(
+                        // color: Colors.grey, // Cor da linha
+                        thickness: 1, // Espessura da linha
+                      ),
+                      const SizedBox(height: 50),
+                      const Center(
+                        child: Text(
+                          '© Honeywell 2025',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
